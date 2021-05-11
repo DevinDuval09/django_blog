5011e0c5071c3f9868861209d08dc0cd1d5eb4b6
Devin Duval <devinduval09@gmail.com>
2021-05-10 20:41:14 -0700

    added Post model

--- django_blog/blogging/models.py
+++ django_blog/blogging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.CharField(blank=True)
+    author = models.ForeignKey(User, on_delete=CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+    post_date = models.DateField(blank=True, null=True)
