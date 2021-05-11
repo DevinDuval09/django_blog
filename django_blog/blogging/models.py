149f8d570690f3b2eb23efee4a578637d24b3afb
Devin Duval <devinduval09@gmail.com>
2021-05-11 09:09:03 -0700

    added import for User

--- django_blog/blogging/models.py
+++ django_blog/blogging/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
