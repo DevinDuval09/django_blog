d8e25dd21592fc3696601eea382e5ce9b4069054
Devin Duval <devinduval09@gmail.com>
2021-05-15 11:07:21 -0700

    added update_user_posts

--- django_blog/blogging/views.py
+++ django_blog/blogging/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Post
+from django.db import transaction
 
 def list_view(request):
     context = {'posts': Post.objects.all()}
@@ -15,3 +16,27 @@ def detail_view(request, post_id):
     
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+def user_view(request, username):
+    
+    posts = Post.objects.all().filter(author__username=username)
+    
+    context = {'posts': posts}
+    print(context)
+    return render(request, 'blogging/list.html', context)
+
+def update_user_posts(request, username, text):
+    #print(request)
+    posts = Post.objects.all().select_for_update().filter(author__username=username)
+    with transaction.atomic():
+        try:
+            assert len(posts) > 0
+            for post in posts:
+                post.text = text
+                post.save()
+        except AssertionError:
+            raise transaction.TransactionManagementError(f'{username} does not have any posts.')
+    
+    context = {'posts': posts, 'username': username}
+    return render(request, 'blogging/detail_list.html', context)
+
