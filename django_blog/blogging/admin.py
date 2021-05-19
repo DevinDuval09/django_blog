ea6349347e7ee8806b5d2dab07687e48da92a361
Devin Duval <devinduval09@gmail.com>
2021-05-18 18:02:49 -0700

    assignment 7 complete

--- django_blog/blogging/admin.py
+++ django_blog/blogging/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Category, Post
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'post_date']
+    inlines = [CategoryInline]
+    ordering = ['title']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name', 'description']
+    ordering = ['name']
+    exclude = ['posts']
+    #actions = [list_categories]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
