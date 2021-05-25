d39022004e409211cb975b8d6b2c27655486ca0f
Devin Duval <devinduval09@gmail.com>
2021-05-25 13:50:23 -0700

    added GenericSortList (currently only works with Post)

--- django_blog/blogging/urls.py
+++ django_blog/blogging/urls.py
@@ -1,8 +1,11 @@
 
-from django.urls import path
-from .views import PostPostedList, stub_view, PostDetail
+from django.urls import path, re_path, translate_url
+from .views import PostPostedList, PostUserPublishedList, stub_view, PostDetail, PostUserList, PostUserPublishedList, GenericSortedList
 
 urlpatterns = [
     path('', PostPostedList.as_view(), name="post_index"),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('posts/<str:username>/', PostUserList.as_view(), name="post_user"),
+    path('posts/<str:username>/published/', PostUserPublishedList.as_view(), name='user_published'),
+    re_path(r"^posts/(?P<command>[A-Za-z]*)/(?P<parameter>[A-Za-z]*_?[A-Za-z]*?_?_?[A-Za-z]*)/(?P<value>[A-Za-z0-9]*|[0-9]{4}-[0-9]{2}-[0-9]{2})/$", GenericSortedList.as_view(), name='post_query')
 ]
--- django_blog/blogging/views.py
+++ django_blog/blogging/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.db.models.query import QuerySet
 from django.http.response import HttpResponse
 from django.shortcuts import render
@@ -15,7 +16,7 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join([f'\t{arg}' for arg in args])
     if kwargs:
         body += "Kwargs:\n"
-        body += '\n'.join([f'\t{key}: {value}' for key, value in kwargs.items()])
+        body += '\n'.join([f'\t{tup}' for tup in kwargs.items()])
     return HttpResponse(body, content_type='text/plain')
 
 
@@ -33,6 +34,25 @@ class PostDetail(DetailView):
     queryset = Post.objects.exclude(post_date=None)
     template_name = 'blogging/detail.html'
 
+
+class PostUserList(ListView):
+    template_name = 'blogging/list.html'
+    def get_queryset(self):
+        return Post.objects.filter(author__username=self.kwargs['username'])
+
+
+class PostUserPublishedList(ListView):
+    template_name = 'blogging/list.html'
+    def get_queryset(self):
+        return Post.objects.filter(author__username=self.kwargs['username']).exclude(post_date=None).order_by('-post_date')
+
+
+class GenericSortedList(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+    def get_queryset(self):
+        kwargs_dict = {self.kwargs['parameter']: self.kwargs['value']}
+        return getattr(self.model.objects, self.kwargs['command'])(**kwargs_dict)
             
 
 
