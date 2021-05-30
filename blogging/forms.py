5d1754dc0c3856bf6a91177638aee0cf2c373b6d
Devin Duval <devinduval09@gmail.com>
2021-05-30 13:13:41 -0700

    author and post fields now readonly

--- blogging/forms.py
+++ blogging/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from logging import disable
+from django.forms import ModelForm, TextInput
 from .models import Comment
 
 
@@ -6,3 +7,7 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ["author", "post", "text"]
+        widgets = {
+            "author": TextInput({"readonly": True}),
+            "post": TextInput({"readonly": True}),
+        }
