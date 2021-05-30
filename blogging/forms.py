94ebe7ffbbe6dc8f37c64b43fae4568626f1d517
Devin Duval <devinduval09@gmail.com>
2021-05-30 15:49:36 -0700

    author and post labels now hidden

--- blogging/forms.py
+++ blogging/forms.py
@@ -8,6 +8,7 @@ class CommentForm(ModelForm):
         model = Comment
         fields = ["author", "post", "text"]
         widgets = {
-            "author": TextInput({"readonly": True}),
-            "post": TextInput({"readonly": True}),
+            "author": TextInput({"readonly": True, "hidden": True}),
+            "post": TextInput({"readonly": True, "hidden": True}),
         }
+        labels = {"text": ("New comment: "), "author": (""), "post": ("")}
