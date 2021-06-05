2b79d07b14d8ab1870526afb1e2c7c3634ea18b1
Devin Duval <devinduval09@gmail.com>
2021-06-05 09:51:00 -0700

    added code to create and edit posts and urls to view published and unpublished posts by user

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,25 +1,34 @@
-from django.urls import path, re_path, translate_url
+from django.urls import path, re_path
 from .views import (
     PostPostedList,
+    PostUserNotPublished,
     PostUserPublishedList,
-    stub_view,
     PostDetail,
     PostUserList,
     PostUserPublishedList,
     GenericSortedList,
     add_comment,
     create_user,
+    create_post,
+    edit_post,
 )
 
 urlpatterns = [
     path("", PostPostedList.as_view(), name="post_index"),
     path("posts/<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("posts/<int:pk>/comments", add_comment, name="comments"),
+    path("posts/<int:pk>/edit", edit_post, name="edit_post"),
+    path("posts/new_post/", create_post, name="create_post"),
     path("posts/<str:username>/", PostUserList.as_view(), name="post_user"),
     path(
         "posts/<str:username>/published/",
         PostUserPublishedList.as_view(),
         name="user_published",
+    ),
+    path(
+        "posts/<str:username>/unpublished",
+        PostUserNotPublished.as_view(),
+        name="user_not_published",
     ),
     re_path(
         r"^posts/(?P<command>[A-Za-z]*)/(?P<parameter>[A-Za-z]*_?[A-Za-z]*?_?_?[A-Za-z]*)/(?P<value>[A-Za-z0-9]*|[0-9]{4}-[0-9]{2}-[0-9]{2})/$",
--- blogging/views.py
+++ blogging/views.py
@@ -1,10 +1,46 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .forms import CommentForm, NewUserForm
+from .forms import CommentForm, NewUserForm, PostForm
 from django.contrib.auth import login
+
+
+def create_post(request, *args, **kwargs):
+    if request.user.id is None:
+        redirect("/login/")
+    form = PostForm(initial={"author": request.user.id})
+    if form.is_valid():
+        post_instance = form.save(commit=False)
+        post_instance.save()
+        return redirect(reverse("post_detail", args=[post_instance.pk]))
+    return render(request,
+                "blogging/new_post.html",
+                {"form": form})
+
+
+def edit_post(request, *args, **kwargs):
+    post = Post.objects.get(pk=kwargs["pk"])
+    if request.user.id is None:
+        return redirect("/login/")
+    if request.user.id != post.author.id:
+        return redirect("/")
+    if request.method == "GET":
+        form = PostForm(instance=post, initial={"title": post.title, "text": post.text, "post_date": post.post_date})
+        return render(request, "blogging/new_post.html", {"form": form, "title": "Edit Post"})
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            updated_post = form.save(commit=False)
+            updated_post.pk = kwargs["pk"]
+            updated_post.save(update_fields=["title", "text", "post_date"])
+            return redirect(reverse("post_detail", args=[post.pk]))
+        else:
+            return stub_view(request, post=form, errors=form.errors)
+    else:
+        return stub_view(request, method=request.method)
 
 
 def stub_view(request, *args, **kwargs):
@@ -20,6 +56,8 @@ def stub_view(request, *args, **kwargs):
 
 def add_comment(request, *args, **kwargs):
     form = CommentForm(request.POST)
+    if request.user.id is None:
+        return redirect("/login/")
     if form.is_valid():
         model_instance = form.save(commit=False)
         model_instance.save()
@@ -56,7 +94,7 @@ class PostPostedList(ListView):
 
 
 class PostDetail(DetailView):
-    queryset = Post.objects.exclude(post_date=None)
+    queryset = Post.objects.all()
     template_name = "blogging/detail.html"
 
     def get_context_data(self, **kwargs):
@@ -97,9 +135,19 @@ class PostUserPublishedList(ListView):
 
     def get_queryset(self):
         return (
-            Post.objects.filter(author__username=self.kwargs["username"])
+            Post.objects.filter(author__pk=self.request.user.id)
             .exclude(post_date=None)
             .order_by("-post_date")
+        )
+
+class PostUserNotPublished(ListView):
+    template_name = "blogging/list.html"
+
+    def get_queryset(self):
+        return (
+            Post.objects.filter(author__pk=self.request.user.id)
+            .filter(post_date__isnull=True)
+            .order_by("-created_date")
         )
 
 
